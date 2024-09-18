0c051e26c3a00371c77753f0ab7539c977c6adf5
ajaykumar <ajaykumarthappeta119@gmail.com>
2024-09-18 23:23:40 +0530

    mongo url change

--- config/db.js
+++ config/db.js
@@ -4,12 +4,18 @@ require("dotenv").config();
 const url = process.env.MONGO_URL;
 
 const connectToDB = mongoose
-  .connect(url)
+  .connect(url,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 10000, // 10 seconds timeout for connection
+    socketTimeoutMS: 45000, // Socket timeout
+    family: 4, // Use IPv4, since IPv6 can cause connection issues
+  })
   .then(() => {
-    console.log("Db Connected Successfully..✅");
+    console.log("Db Connected Successfully..✅"); 
   })
   .catch((error) => {
-    console.log("Faiiled To Connect Db...❌");
-  });
+    console.log(console.log(error.message),"Faiiled To Connect Db...❌");
+  }); 
 
 module.exports = connectToDB;
